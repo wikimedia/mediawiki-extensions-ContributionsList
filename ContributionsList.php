061166b48b27f32cd8627735e169844da56d2d43
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-01-26 13:37:46 +0100

    Convert ContributionsList to use extension registration
    Bug: T185749
    Change-Id: Id6a77a1c3489197b592b49feaaf3426b3638e8ce

--- ContributionsList.php
+++ ContributionsList.php
@@ -1,29 +1,32 @@
 <?php
 /**
- * ContributionsList extension
+ * MediaWiki extension ContributionsList
  *
- * For more info see http://mediawiki.org/wiki/Extension:ContributionsList
+ * Copyright (C) 2014, Ike Hecht
  *
- * @file
- * @ingroup Extensions
- * @author Ike Hecht, 2014
- * @license GNU General Public Licence 2.0 or later
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'ContributionsList',
-	'author' => array(
-		'Ike Hecht',
-	),
-	'version' => '0.1.0',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:ContributionsList',
-	'descriptionmsg' => 'contributionslist-desc',
-);
-
-$wgAutoloadClasses['ContributionsListHooks'] = __DIR__ . '/ContributionsList.hooks.php';
-$wgAutoloadClasses['ContributionsList'] = __DIR__ . '/ContributionsList.class.php';
-
-$wgMessagesDirs['ContributionsList'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['ContributionsListMagic'] = __DIR__ . '/ContributionsList.magic.php';
-
-$wgHooks['ParserFirstCallInit'][] = 'ContributionsListHooks::setupParserFunction';
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'ContributionsList' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['ContributionsList'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the ContributionsList extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the ContributionsList extension requires MediaWiki 1.25+' );
+}
